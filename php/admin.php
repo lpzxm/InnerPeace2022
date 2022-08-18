04832a0a342ddf7a59c76a0c96acdf106d1aa374
Carlos Merino <merinoventura123@gmail.com>
2022-08-18 16:42:01 -0600

    Hoy si XD

--- php/admin.php
+++ php/admin.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Panel de el administrador (Prototipo</title>
+    <title>Panel de el administrador</title>
     <link rel="stylesheet" href="../css/Dashboard.css">
 </head>
 
@@ -52,6 +52,8 @@
                 </li>
             </ul>
         </div>
+
+        
 
        
         <div class="main">
@@ -102,16 +104,115 @@
             </div>
 
          
-     }
+            <div class="details">
+                <div class="recentOrders">
+                    <div class="cardHeader">
+                        <h2 id="Link7">Usos recientes</h2>
+                        <a href="#" class="btn" id="Link8">Mirar todo</a>
+                    </div>
+
+                    <?php 
+
+include("../php/auth/db.php")
+
+?>
+
+<br>
+
+<table border="1" >
+    <tr>
+        <td>id</td>
+        <td>nombre</td>
+        <td>Especialidad</td>
+        <td>Codigo de el estudiante</td>
+        <td>Año</td>	
+        <td>Seccion</td>	
+        <td>Fecha de nacimiento</td>	
+    </tr>
+
+    <?php 
+    $sql="SELECT * from datos_estudiante";
+    $result=mysqli_query($con,$sql);
+
+    while($mostrar=mysqli_fetch_array($result)){
+     ?>
+
+    <tr>
+        <td><?php echo $mostrar['id'] ?></td>
+        <td><?php echo $mostrar['Nombre'] ?></td>
+        <td><?php echo $mostrar['Especialidad'] ?></td>
+        <td><?php echo $mostrar['Codigo_estudiante'] ?></td>
+        <td><?php echo $mostrar['Año'] ?></td>
+        <td><?php echo $mostrar['Seccion'] ?></td>
+        <td><?php echo $mostrar['Fecha_nacimiento'] ?></td>
+
+    </tr>
+<?php 
+}
+ ?>
+</table>
+
+
+      </div>
+
+               
+                <div class="recentCustomers">
+                    <div class="cardHeader">
+                        <h2 id="Link15">Usuarios recientes </h2>
+                    </div>
+
+                    <table>
+                        <tr>
+                            <td width="60px">
+                                <div class="imgBx"><img src="../img/customer02.jpg" alt=""></div>
+                            </td>
+                            <td>
+                                <h4>Eduardo <br> <span>DSW</span></h4>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td width="60px">
+                                <div class="imgBx"><img src="../img/customer01.jpg" alt=""></div>
+                            </td>
+                            <td>
+                                <h4>Guevara<br> <span id="Link16">Diseño grafico</span></h4>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td width="60px">
+                                <div class="imgBx"><img src="../img/customer02.jpg" alt=""></div>
+                            </td>
+                            <td>
+                                <h4>Lara <br> <span>DSW</span></h4>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td width="60px">
+                                <div class="imgBx"><img src="../img/customer01.jpg" alt=""></div>
+                            </td>
+                            <td>
+                                <h4>Aguirre<br> <span>Emca</span></h4>
+                            </td>
+                        </tr>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    
 
          <div class="translatebutton">
                 <input type="checkbox" name="translate" id="translate">
              <label for="translate" class="trans"></label>
          </div>
 
-
+<!--
     <script src="../js/Dashboard.js"></script>
-    <script src="../js/admin.js"></script>
+    <script src="../js/admin.js"></script>*/ -->
 
 
    
